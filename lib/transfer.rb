a214b0d5f4dcf78cb1358bfaa06ee5f2399bf002
Daniel Schneider <daschne8@gmail.com>
2019-03-26 16:29:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
       if @sender.balance >= @amount
         @sender.balance -= @amount
         @receiver.balance += @amount
-        @status = "complete"
+        @status = "completed"
       else
         @status = "rejected"
         "Transaction rejected. Please check your account balance."
