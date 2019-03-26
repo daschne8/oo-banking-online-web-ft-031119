1d382d9ef274d8f849ef9f8a12477a2fbdbc95c6
Daniel Schneider <daschne8@gmail.com>
2019-03-26 15:16:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
-  attr_accessor  :status
-  attr_reader :sender, :receiver
+  attr_accessor  :status, :receiver
+  attr_reader :sender
   def initialize(sender,reciever,amount)
     @sender,@receiver = sender,receiver
     @status = "pending"
